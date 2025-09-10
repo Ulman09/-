abb678dc5eff2aa8ac3d269687ec3499caf8aaed
Ulman09 <popov.alex.i@yandex.ru>
2025-09-10 13:57:33 +0300

    Add files via upload

---
+++ task1-2.py
@@ -0,0 +1,10 @@
+#Представленние массива данных
+A = ['mp3','mp4','jpeg','png','7z']
+
+
+#Организация структуры данных стэк
+stack =
+stack.append('a')
+stack.append('b')
+stack.append('c')
+print(stack)
