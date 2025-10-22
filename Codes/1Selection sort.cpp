00bdbf2abebbd721b4ea00990c6d2567bd269d0a
Ulman09 <popov.alex.i@yandex.ru>
2025-10-22 13:34:33 +0300

    Create 1Selection sort.cpp

---
+++ Codes/1Selection sort.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+void selection_sort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n; ++i) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        swap(arr[i], arr[min_idx]);
+    }
+}
