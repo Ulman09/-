1bf229c954cfd7fdaaad9cb1ffefd77a738f1ac0
Ulman09 <popov.alex.i@yandex.ru>
2025-10-22 13:38:03 +0300

    Create 6Quick sort.cpp

---
+++ Codes/6Quick sort.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; ++j) {
+        if (arr[j] <= pivot) {
+            ++i;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quick_sort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
