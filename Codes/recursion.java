b9f4b478dc86fb18af6a7d8ec4a1ec7dd4774159
Ulman09 <popov.alex.i@yandex.ru>
2025-11-05 13:14:34 +0300

    Create recursion.java

---
+++ Codes/recursion.java
@@ -0,0 +1,24 @@
+public class Power {
+    public static double power(double x, int n) {
+        if (n == 0) return 1;
+        if (n < 0) return 1 / power(x, -n);
+        if (n % 2 == 0) {
+            double half = power(x, n / 2);
+            return half * half;
+        } else {
+            return x * power(x, n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        double base = 2;
+        int exp = 5;
+        double result = power(base, exp);
+        System.out.println(base + "^" + exp + " = " + result);
+        
+        base = 3;
+        exp = -2;
+        result = power(base, exp);
+        System.out.println(base + "^" + exp + " = " + result);
+    }
+}
